b15f3d47db21dba3f8f89b9589e74d4e67010a82
Linus Karlsson <linus@zozs.se>
2018-03-07 20:48:03 +0100

    Can now show stats for multiple quizzes.
    Also show attempts, attempts before pass, and scores in HTML.

--- db/moodle-parser.js
+++ db/moodle-parser.js
@@ -3,30 +3,40 @@ const fs = require('fs')
 const moment = require('moment')
 const { promisify } = require('util')
 
+const QUIZ_PASS_LIMIT = 8.0 // TODO: do not hard code limit...
+
 function getQuizFilenames () {
   // Read all quiz filenames from environmental variables QUIZ1, QUIZ2, and QUIZ3.
   // TODO: hard-coded to three quizes and a single file for each.
   const quizVars = [process.env.QUIZ1, process.env.QUIZ2, process.env.QUIZ3]
   return quizVars
-    .map((f, i) => ({'quiz': i + 1, 'filename': f}))
+    .map((f, i) => ({quiz: i + 1, filename: f}))
     .filter(q => q.filename !== undefined)
 }
 
-function groupAttempts (attempts) {
-  // Returns a list of people, where the attempts are added to each person.
+function groupAttempts (quizzes, attempts) {
+  // Returns a list of people, where the attempts are added to each person and quiz.
+  // Make sure that all quizzes (even if no attempts) exists on all users.
   // Start by using e-mail as key, later on we return a list of objects as expected.
+  const quizBase = {}
+  for (let quiz of quizzes) {
+    quizBase[quiz.quiz] = 0
+  }
   let emails = {}
   for (let attempt of attempts) {
     const email = attempt.person.email
     if (email) {
       if (!emails.hasOwnProperty(email)) {
         emails[email] = {...attempt.person} // clone person object
-        emails[email].attempts = []
+        emails[email].quizzes = {...quizBase}
+        for (let quiz of quizzes) {
+          emails[email].quizzes[quiz.quiz] = []
+        }
       }
 
       // Also sort attempts based on starting time.
-      emails[email].attempts.push(attempt.attempt)
-      emails[email].attempts.sort((a, b) => a.started.unix() - b.started.unix())
+      emails[email].quizzes[attempt.attempt.quiz].push(attempt.attempt)
+      emails[email].quizzes[attempt.attempt.quiz].sort((a, b) => a.started.unix() - b.started.unix())
     } else {
       // No e-mail address for this user.
       throw Error('No email for person: ' + JSON.stringify(attempt.person))
@@ -37,46 +47,51 @@ function groupAttempts (attempts) {
   return Object.values(emails)
 }
 
-async function parseQuizFile (filename) {
+async function parseQuiz (quiz) {
   const parseOptions = {
     columns: true,
     delimiter: ','
   }
 
-  let input = await promisify(fs.readFile)(filename, 'utf8')
-  let data = await promisify(csvParser)(input, parseOptions)
-  return data
-    .map(transformCsvEntry)
+  let input = await promisify(fs.readFile)(quiz.filename, 'utf8')
+  let csvData = await promisify(csvParser)(input, parseOptions)
+  // TODO: filter on finished attempts?
+  let data = csvData
+    .map(row => transformCsvEntry(row, quiz.quiz))
     .filter(a => a.person.lastname !== 'Overall average')
+  return data
 }
 
-function transformCsvEntry (entry) {
+function transformCsvEntry (entry, quiz) {
   const dateFormat = 'DD MMM YYYY hh:mm a'
   return {
-    'person': {
-      'lastname': entry['Surname'],
-      'firstname': entry['First name'],
-      'email': entry['Email address']
+    person: {
+      lastname: entry['Surname'],
+      firstname: entry['First name'],
+      email: entry['Email address']
     },
-    'attempt': {
-      'state': entry['State'],
-      'started': moment(entry['Started on'], dateFormat),
-      'ended': moment(entry['Completed'], dateFormat),
-      'duration': entry['Time taken'], // TODO: parse duration?
-      'score': parseFloat(entry['Grade/10.00'])
+    attempt: {
+      quiz: quiz,
+      state: entry['State'],
+      started: moment(entry['Started on'], dateFormat),
+      ended: moment(entry['Completed'], dateFormat),
+      // 'duration': entry['Time taken'], // TODO: parse duration?
+      score: parseFloat(entry['Grade/10.00']),
+      passed: parseFloat(entry['Grade/10.00']) >= QUIZ_PASS_LIMIT
     }
   }
 }
 
 module.exports = {
-  quizData: () => {
-    let quizzes = getQuizFilenames()
-    // TODO: filter on finished attempts?
-    return parseQuizFile(quizzes[0].filename)
-      .then(groupAttempts)
-      .catch(e => {
-        console.log('Exception in parser:', e)
-        throw Error('Exception in parser: ' + JSON.stringify(e))
-      })
+  quizData: async () => {
+    try {
+      let quizzes = getQuizFilenames()
+      let quizData = await Promise.all(quizzes.map(parseQuiz))
+      let groupedData = groupAttempts(quizzes, [].concat(...quizData))
+      return groupedData
+    } catch (e) {
+      console.log('Exception in parser:', e)
+      throw Error('Exception in parser: ' + JSON.stringify(e))
+    }
   }
 }
--- static/index.js
+++ static/index.js
@@ -1,3 +1,5 @@
+/* global $ */
+
 function formatDetails (d) {
   let detailsTbl = $('<table/>').addClass('detailed')
   let emailRow = $('<tr/>')
@@ -6,6 +8,19 @@ function formatDetails (d) {
   detailsTbl.append(emailRow)
   return detailsTbl
 }
+
+const twoFixed = func => (data, type) => type === 'display' || type === 'filter' ? func(data).toFixed(2) : func(data)
+const onlyPositive = func => (data, type) => type === 'display' || type === 'filter' ? (!isNaN(func(data)) ? func(data).toString() : 'N/A') : (!isNaN(func(data)) ? func(data) : 6000)
+
+const quizAttemptsCount = attempts => attempts ? attempts.length : 0
+const quizAttemptsBeforePass = attempts => attempts && attempts.findIndex(a => a.passed) >= 0 ? attempts.findIndex(a => a.passed) + 1 : NaN
+const quizAttemptsBeforePassCount = onlyPositive(quizAttemptsBeforePass)
+const quizMaxScore = attempts => attempts ? Math.max(0, ...attempts.map(a => a.score)) : 0
+const quizMaxScoreFmt = twoFixed(quizMaxScore)
+
+const allQuizAttemptsCount = quizzes => Object.values(quizzes).map(quizAttemptsCount).reduce((a, b) => a + b)
+const allQuizAttemptsBeforePassCount = onlyPositive(quizzes => Object.values(quizzes).map(quizAttemptsBeforePass).reduce((a, b) => a + b))
+const allQuizMaxScoreSumFmt = twoFixed(quizzes => Object.values(quizzes).map(quizMaxScore).reduce((a, b) => a + b))
 
 $(document).ready(function () {
   let table = $('#quizTable').DataTable({
@@ -20,23 +35,24 @@ $(document).ready(function () {
       { data: 'firstname' },
       { data: 'lastname' },
 
-      { data: row => row.attempts.length },
-      { data: row => 0 },
-      { data: row => 0 },
+      { className: 'dt-right', data: 'quizzes.1', render: quizAttemptsCount },
+      { className: 'dt-right', data: 'quizzes.1', render: quizAttemptsBeforePassCount },
+      { className: 'dt-right', data: 'quizzes.1', render: quizMaxScoreFmt },
 
-      { data: row => row.attempts.length },
-      { data: row => 0 },
-      { data: row => 0 },
+      { className: 'dt-right', data: 'quizzes.2', render: quizAttemptsCount },
+      { className: 'dt-right', data: 'quizzes.2', render: quizAttemptsBeforePassCount },
+      { className: 'dt-right', data: 'quizzes.2', render: quizMaxScoreFmt },
 
-      { data: row => row.attempts.length },
-      { data: row => 0 },
-      { data: row => 0 },
+      { className: 'dt-right', data: 'quizzes.3', render: quizAttemptsCount },
+      { className: 'dt-right', data: 'quizzes.3', render: quizAttemptsBeforePassCount },
+      { className: 'dt-right', data: 'quizzes.3', render: quizMaxScoreFmt },
 
-      { data: row => row.attempts.length },
-      { data: row => 0 },
-      { data: row => 0 }
+      { className: 'dt-right', data: 'quizzes', render: allQuizAttemptsCount },
+      { className: 'dt-right', data: 'quizzes', render: allQuizAttemptsBeforePassCount },
+      { className: 'dt-right', data: 'quizzes', render: allQuizMaxScoreSumFmt }
     ],
-    order: [[1, 'asc']]
+    order: [[1, 'asc']],
+    lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, 'All']]
   })
 
   // Open/close details event handler.
